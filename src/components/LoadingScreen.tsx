5ca8ce54ff940aaac98438f55a7a23cd1f4894a3
takibi <120891266+Takibiueta@users.noreply.github.com>
2025-08-08 21:40:13 +0900

    Extend final waiting time to 2.5 seconds

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -129,7 +129,7 @@ const LoadingScreen = ({ onComplete }: LoadingScreenProps) => {
                 const completeTimer = setTimeout(() => {
                   setPhase(6);
                   onComplete();
-                }, 1500);
+                }, 2500);
                 timerIds.push(completeTimer);
               }, 1000); // 「美しく」のフェード完了(1秒)を待つ
             }
